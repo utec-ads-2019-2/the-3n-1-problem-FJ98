0979e1fdad38934d2ff9aabc34b1cf6c6643d222
FJ98 <felix.solano@utec.edu.pe>
2019-08-26 03:10:51 -0500

    3n + 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,49 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-using namespace std;
+void orderInput(int&, int&);
+int getMaxCycle(int, int);
+int threeNPlusOne(int);
+void print(int, int, int);
 
-int main(int argc, char *argv[]) {
+int main() {
+    int inferior, superior;
+
+    while (std::cin >> inferior >> superior) {
+        int maxCycle = getMaxCycle(inferior, superior);
+        print(inferior, superior, maxCycle);
+    }
+
     return 0;
+}
+
+void orderInput(int& inferior, int& superior){
+    if (inferior > superior)
+        std::swap(inferior, superior);
+}
+
+int getMaxCycle(int inferior, int superior){
+    orderInput(inferior, superior);
+    int maxCycle = 0;
+
+    for (int i = inferior; i <= superior; ++i) {
+        int cycle = threeNPlusOne(i);
+        if (cycle > maxCycle) std::swap(cycle, maxCycle);
+    }
+
+    return maxCycle;
+}
+
+int threeNPlusOne(int number){
+    int countSteps = 1;
+
+    while (number != 1) {
+        number = (number % 2 == 0) ? number / 2 : 3*number + 1;
+        ++countSteps;
+    }
+
+    return countSteps;
+}
+
+void print(int inferior, int superior, int maxCycle){
+    std::cout << inferior << " " << superior << " " << maxCycle << std::endl;
 }
